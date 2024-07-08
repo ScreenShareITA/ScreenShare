743e8a106652101ecc072f6e7ea2deb6864c45de
Katoylla <omarboh8@gmail.com>
2024-07-09 00:49:36 +0200

    Update SSITAPCAdb.ps1

--- SSITAPCAdb.ps1
+++ SSITAPCAdb.ps1
@@ -1,5 +1,21 @@
+$ErrorActionPreference="SilentlyContinue"
+write-host -f red "\n ____  ____  __  ____  __     ____   ___   __   ____  ____ 
+/ ___)/ ___)(  )(_  _)/ _\   (  _ \ / __) / _\ (    \(  _ \
+\___ \\___ \ )(   )( /    \   ) __/( (__ /    \ ) D ( ) _ (
+(____/(____/(__) (__)\_/\_/  (__)   \___)\_/\_/(____/(____/\n"
+write-host -n "Made by ";write-host -f blue -n "Katoylla ";write-host -n "for ";write-host -f red -n "SSITA";write-host -n "(";write-host -f red -n "https://discord.gg/ssita";write-host ")"
 $res = [System.Collections.Generic.List[PSObject]]::new()
-$raw = (gc "$env:windir\appcompat\pca\PcaGeneralDb0.txt") -split "\n"
+$p="$env:windir\appcompat\pca\PcaGeneralDb0.txt"
+if(!(test-path $p)){
+  write "File non trovato nella path: $p"
+  if(test-path "$env:windir\appcompat\pca"){
+    write "Cartella pca trovata verifica se i file sono stati eliminati o ne è stato modificato il nome"
+  }else{
+    write "Cartella pca non presente. Probabilmente dipende dal pc dell'utente prosegui con l'ss"
+  }
+  exit
+}
+$raw = (gc $p -raw) -split "\r?\n"
 $raw|%{
   $curr=$_ -split "\|";
   $res.add([pscustomobject]@{
