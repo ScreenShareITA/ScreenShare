aae56db071b189426d6e6128394c3e42f9c648f2
Katoylla <omarboh8@gmail.com>
2024-07-08 22:02:51 +0200

    Update ZunemusicSSITA.ps1

--- ZunemusicSSITA.ps1
+++ ZunemusicSSITA.ps1
@@ -1,4 +1,5 @@
 $res=@()
+//meglio mettere $res = [System.Collections.Generic.List[PSObject]]::new() e poi fare .add()
 $dt=gci "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.ZuneMusic_8wekyb3d8bbwe\PersistedStorageItemTable\ManagedByApp"
 $dt|%{
   $path=$_.GetValue('FilePath')
