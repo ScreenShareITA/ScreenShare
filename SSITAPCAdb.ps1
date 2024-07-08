39a2bbcafd22783fd72181e3da3352ccc2b2aa3d
Katoylla <omarboh8@gmail.com>
2024-07-08 22:15:21 +0200

    fix SSITAPCAdb.ps1

--- SSITAPCAdb.ps1
+++ SSITAPCAdb.ps1
@@ -2,7 +2,7 @@ $res = [System.Collections.Generic.List[PSObject]]::new()
 $raw = (gc "$env:windir\appcompat\pca\PcaGeneralDb0.txt") -split "\n"
 $raw|%{
   $curr=$_ -split "\|";
-  $res.add[pscustomobject]@{
+  $res.add([pscustomobject]@{
     ExecutionTime=$curr[0]
     RunStatus=$curr[1]
     Path=$curr[2]
@@ -11,6 +11,6 @@ $raw|%{
     FileVersion=$curr[5]
     AmcacheProgramID=$curr[6]
     ExitCode=$curr[7]
-  }
+  })
 }
 $res|ogv -t "PCA General DataBase Parser | Made by Katoylla for SSITA (https://discord.gg/ssita) | values parsed: $($res.count)" -passthru
