f3818d147192c95c40db2470f6dc4790f8846d3f
Katoylla <omarboh8@gmail.com>
2024-07-09 00:50:09 +0200

    Create made.ps1

---
+++ made.ps1
@@ -0,0 +1,7 @@
+write-host -n "Made by ";
+write-host -f blue -n "Katoylla ";
+write-host -n "for ";
+write-host -f red -n "SSITA";
+write-host -n "(";
+write-host -f red -n "https://discord.gg/ssita";
+write-host ")"
