9bb49dddd117a0826fa93854b6a42ff09d8d706c
Katoylla <omarboh8@gmail.com>
2024-07-09 06:03:37 +0200

    Update SSITAPCAdb.ps1

--- SSITAPCAdb.ps1
+++ SSITAPCAdb.ps1
@@ -21,17 +21,18 @@ if(!(test-path $p)){
 $raw = (gc $p -raw) -split "\r?\n"
 $raw|%{
   $curr=$_ -split "\|";
+  $curr[2]=[System.IO.Path]::GetFullPath([System.Environment]::ExpandEnvironmentVariables($curr[2]))
+  $test=test-path $curr[2]
   $res.add([pscustomobject]@{
     ExecutionTime=$curr[0]
     RunStatus=$curr[1]
-    if($curr[2] -match "^%"){
-     Path=[System.IO.Path]::GetFullPath([System.Environment]::ExpandEnvironmentVariables($curr[2]))
-    }else{Path=$curr[2]}
+    Path=$curr[2]
     FileDescription=$curr[3]
     SoftwareVendor=$curr[4]
     FileVersion=$curr[5]
     AmcacheProgramID=$curr[6]
     ExitCode=$curr[7]
+    if()
   })
 }
 $res|ogv -t "PCA General DataBase Parser | Made by Katoylla for SSITA (https://discord.gg/ssita) | values parsed: $($res.count)" -passthru
