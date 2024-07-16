a588d7e8bec9932393075ed5d0e8195016ef0d1c
Katoylla <omarboh8@gmail.com>
2024-07-16 20:10:00 +0200

    Update SSITAFivemModules.ps1

--- SSITAFivemModules.ps1
+++ SSITAFivemModules.ps1
@@ -1,4 +1,5 @@
 $fp=ps|?{$_.name -like "fivem*"}
 $fd=$fp|%{ps -id $_.id -m}
 $dlls=(irm https://raw.githubusercontent.com/Katoylla/ScreenShare/main/dllsus.txt) -split "\n"
-write $fd
+$sus=$fd|?{$_ -notin $dlls}
+write $sus 
