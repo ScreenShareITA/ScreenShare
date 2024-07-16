b43a88144b731ca17acda4b80206493d5353a584
Katoylla <omarboh8@gmail.com>
2024-07-16 20:16:26 +0200

    Update SSITAFivemModules.ps1

--- SSITAFivemModules.ps1
+++ SSITAFivemModules.ps1
@@ -2,5 +2,5 @@ $fp=ps|?{$_.name -like "fivem*"}
 $fd=$fp|%{ps -id $_.id -m}
 $dlls=(irm https://raw.githubusercontent.com/Katoylla/ScreenShare/main/dllsus.txt) -split "\n"
 $normdll=$dlls|%{[System.Environment]::ExpandEnvironmentVariables($_)}
-$sus=$fd|?{$_ -notin $normdll}
-write $sus 
+$sus=$fd|?{$_.filename -notin $normdll}
+write $sus
