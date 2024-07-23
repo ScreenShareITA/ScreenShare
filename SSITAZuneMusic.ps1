478a0fcff680936543d8b725fe6d24050ec51521
Katoylla <omarboh8@gmail.com>
2024-07-23 21:44:01 +0200

    Update SSITAZuneMusic.ps1

--- SSITAZuneMusic.ps1
+++ SSITAZuneMusic.ps1
@@ -1,12 +1,12 @@
-$res=@()
-//meglio mettere $res = [System.Collections.Generic.List[PSObject]]::new() e poi fare .add()
+$res=[System.Collections.Generic.List[PSObject]]::new()
+# meglio mettere $res = [System.Collections.Generic.List[PSObject]]::new() e poi fare .add()
 $dt=gci "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\Microsoft.ZuneMusic_8wekyb3d8bbwe\PersistedStorageItemTable\ManagedByApp"
 $dt|%{
   $path=$_.GetValue('FilePath')
-  $res+=[pscustomobject]@{
+  $res.add([pscustomobject]@{
     Path=$path;
     Date=[DateTime]::FromFileTime([Convert]::ToInt64((($_.GetValue('LastUpdatedTime')|% { "{0:X2}" -f $_ })[-1..-8] -join '') , 16));
     Source=$_.Name
-  }
+  })
 }
 $res|ogv -t "ZuneMusic Artifact Parser | Made by Katoylla for SSITA (https://discord.gg/ssita) | values parsed: $($res.count)" -passthru
